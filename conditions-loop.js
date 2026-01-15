f49a65fc8c67135ee6a40074006f8b2ceda572db
KahafSameer <sameerkahaf920@gmail.com>
2026-01-15 15:03:30 +0500

    add data

---
+++ conditions-loop.js
@@ -0,0 +1,12 @@
+let a =10, b=20
+
+if(a>b){
+    console.log('a is greater than b')
+}
+else{
+    console.log('b is greater than a')
+}
+
+for(let i=1;i<=5;i++){
+    console.log(i)
+}
---
+++ objects.js
@@ -0,0 +1,8 @@
+let me = {
+    name : 'kahaf',
+    age : 22,
+    job: 'Devops Engineer',
+    isactive : true,
+    
+}
+console.log(me.name)
