9b2e0983e0306c9d469064b227d8b761d856c6e4
kahaf sameer <159644348+KahafSameer@users.noreply.github.com>
2026-01-20 14:48:45 +0500

    error handler

---
+++ plan-day-3/error-handling.js
@@ -0,0 +1,46 @@
+//try catch block
+// try {
+//     const result = riskyOperation();
+//     console.log(result);
+// } catch (error){
+// console.error("error ocured: ", error.message);
+
+// const { json } = require("express");
+
+// }
+
+
+//asynchronous error handling (promises)
+// doSomething()
+//     .then(result => console.log(result))
+//     .catch(error => console.error("error: ", error.message));
+
+
+// try {
+//     let a =10, b=12;
+//     if(b===0) throw new Error("Division by zero is not null");
+//     console.log(a/b);
+
+// }catch(error){
+//     console.error("catching an error:", error.message);
+// }
+
+
+//Json Parsing
+// try {
+//     let data = '{"name" : "kahaf"}';
+//     let obj = JSON.parse(data);
+//     console.log(obj.name);
+
+//     let wrongdata = "{name : kahaf}";
+//     let obj2 = JSON.parse(wrongdata);
+//     console.log(obj2.name);
+// }catch(error){
+//     console.error("JSON error is : ", error.message);
+// }
+
+
+
+//file handling for try catch error handling
+
+
