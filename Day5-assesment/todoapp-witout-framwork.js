4f0948322f5f348fb9462ea74083709a4270b2b1
kahaf sameer <159644348+KahafSameer@users.noreply.github.com>
2026-01-21 13:21:47 +0500

    todo app (new feature -> UPDATE

--- Day5-assesment/todoapp-witout-framwork.js
+++ Day5-assesment/todoapp-witout-framwork.js
@@ -8,7 +8,8 @@ while(true){
     console.log("1. ADD Task");
     console.log("2. View Task");
     console.log("3. Delete Task");
-    console.log("4. Exit Task\n");
+    console.log("4. Update Task");
+    console.log("5. Exit Task\n");
 
     let choice = parseInt(prompt("Enter Your Choice (1-4)"));
 
@@ -44,7 +45,25 @@ while(true){
         console.log("__Task Deleted__\n")
     }
 
-    else if(choice == 4){
+    else if (choice ==4){
+        index=1;
+        console.log("\nYour tasks are :")
+        for (let value of TODO){
+            console.log(index,".","--",value,"--");
+            index++;
+        }
+        let del_task = parseInt(prompt("\nEnter Task Number to Update: "));
+        del_task--;
+        max_lenth= TODO.length-1;
+        if(del_task>max_lenth && del_task<0){
+            console.log("Envalid Choice &#$&#\n");
+        }
+        let Utask = prompt("Enter your Updated task: ");
+        TODO[del_task]= Utask;
+        console.log("__Task Updated__\n")
+    }
+
+    else if(choice == 5){
         break;
     }
 
